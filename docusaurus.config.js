ecb6b557cc9252abfa04dc9704ad6bea4a13a776
AzureAD\RedaCharf <reda.charf@tpaspa.it>
2024-07-26 12:22:08 +0200

    cambiati colori

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tpa Docs',
+  tagline: 'TPA Products Documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
